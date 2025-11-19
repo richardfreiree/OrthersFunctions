f1bc57874255edff50ce04c5c46d9c1e4c11c73e
richardfreiree <richardzzfreirezz@gmail.com>
2025-11-18 22:15:52 -0300

    sobe sobe

--- extime.php
+++ extime.php
@@ -9,27 +9,40 @@
 <body>
 
 	<h1>Escolha o melhor time do Mundo</h1>
+		<div class="corinthians">
 		<form method="get" action="extime2.php">
 		<div class="escolha">
+
+
+		<div class="corinthians">
+		<img src="img/corinthains.png">
 		<input type="radio" id="corinthians" name="time" value="corinthians"></input>
 		<label for="corinthians">Corinthians</label>
-
+		</div>
+	
 
 			<div class="palmeiras">
 						<img src="img/palmeiras.png">
 				<input type="radio" id="palmeiras" name="time" value="palmeiras"></input>
 		<label for="Palmeiras">Palmeiras</label>
-
 		</div>
 
+
+			<div class="saopaulo">
 				<input type="radio" id="paulo" name="time" value="paulo"></input>
 		<label for="paulo">São Paulo</label>
+		</div>
 
 
+		<div class="boca">
 				<input type="radio" id="boca" name="time" value="boca"></input>
 		<label for="boca">Boca Juniors</label>
+			</div>
 
+
+			<div class="river">
 				<input type="radio" id="river" name="time" value="river"></input>
+				<div/>
 				<div/>
 		<label for="river">River Plate</label>
 		<br>
